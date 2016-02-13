75d4e07aa96c0c770e8ccb75760072975ffd67a0
Stanislas Rolland <typo3@sjbr.ca>
2016-02-13 12:53:33 -0500

    [TASK] Update version to 2.3.1
    Change-Id: I8283fd53a91515658abe6d3ce5b78042672ba790
    Reviewed-on: https://review.typo3.org/46647
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'freeCap CAPTCHA',
 	'description' => 'A TYPO3 integration of freeCap CAPTCHA.',
 	'category' => 'plugin',
-	'version' => '2.3.0',
+	'version' => '2.3.1',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
 	array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.2.0-7.99.99'
+			'typo3' => '6.2.0-7.6.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array()
