21040479c1d832b3a54385b83c2c7b2c488d0154
Stanislas Rolland <typo3@sjbr.ca>
2017-01-07 11:29:36 -0500

    [TASK] Require TYPO3 CMS 7.6 LTS
    Change-Id: I4183c5af60e1e2eb29919040b12c3dcdec7ce59d
    Reviewed-on: https://review.typo3.org/51180
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,11 +15,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Stanislas Rolland',
 	'author_email' => 'typo3(arobas)sjbr.ca',
 	'author_company' => 'SJBR',
-	'constraints' => 
-	array(
+	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.2.0-7.6.99'
+			'typo3' => '7.6.0-8.99.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array()
