d57f6fc761daa8a26ae661a1b2c670c52e152faa
Stanislas Rolland <typo3@sjbr.ca>
2017-10-16 23:19:06 -0400

    [BUGFIX] Migrate from mcrypt to openssl
    mcrypt is deprecated as of PHP 7.1
    
    Resolves: #82716
    Change-Id: I0a58f52309b107571f70a950a28e03aef28fcce7
    Reviewed-on: https://review.typo3.org/54411
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'SJBR',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-8.99.99'
+			'typo3' => '7.6.0-8.7.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array()
